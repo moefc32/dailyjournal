aaea6be2a9a2730520cd8fa329215ee6236e9668
Faizal Chan <moefc32@gmail.com>
2025-02-14 10:14:35 +0700

    optimize the hooks, check for invalid JWT

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,27 +1,30 @@
 import { VITE_APP_NAME } from '$env/static/private';
-import { redirect } from '@sveltejs/kit';
+import { json, redirect } from '@sveltejs/kit';
 import { validateToken } from '$lib/server/token';
+import { checkIsUserExists } from '$lib/server/prisma';
 
 export const handle = async ({ event, resolve }) => {
-    const currentPath = event.url.pathname;
-    const cookies = event.cookies;
+    const { cookies, request, url } = event;
+    const currentPath = url.pathname;
 
     const isTokenValid = validateToken(cookies);
+    const isUserExists = isTokenValid
+        && (await checkIsUserExists(isTokenValid.id));
 
-    if (!isTokenValid) {
+    if (!isTokenValid || !isUserExists) {
         cookies.delete('access_token', { path: '/' });
     }
 
-    const authPaths = ['/login', '/register'];
-    const isJson = event.request.headers
+    const isJson = request.headers
         .get('accept')
         ?.includes('application/json');
 
+    const authPaths = ['/login', '/register'];
     if (authPaths.some((path) => currentPath.startsWith(path))) {
         if (!isJson && isTokenValid) throw redirect(303, '/');
     } else {
         if (!isTokenValid) throw redirect(303, '/login');
     }
 
-    return await resolve(event);
+    return resolve(event);
 }
--- src/lib/server/prisma.js
+++ src/lib/server/prisma.js
@@ -3,3 +3,14 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 export default prisma;
+
+export async function checkIsUserExists(userId) {
+    if (!userId) return false;
+
+    const user = await prisma.user.findUnique({
+        where: { id: userId },
+        select: { id: true },
+    });
+
+    return !!user;
+}
